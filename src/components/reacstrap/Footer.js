1ddada516a39bea1cc86ed6d64f316ec4966a0a7
Adalid <witch_ada@hotmail.com>
2018-04-24 12:10:44 -0500

    Changes

---
+++ src/components/reacstrap/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Footer extends  React.Component {
+    render(){
+        return(
+     <footer>
+         <p> &#169; 2018 LABORATORIA</p>
+     </footer>
+    )
+    }
+}
+ export default Footer;
