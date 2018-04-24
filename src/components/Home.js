d38a820eedfac2240f13f3ca66aaf27501246e77
bernkaztel <bernkaztel@hotmail.com>
2018-04-24 00:03:07 -0500

    subiendo envío de token y comunicación con conekta y post a API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,150 @@
 import React, { Component } from "react";
 import JumbotronCard from "./reacstrap/JumbotronCard";
-import { Button } from 'reactstrap';
-import { Container, Row, Col } from 'reactstrap';
-import {connect} from 'react-redux'
-import {googleLogin} from '../actions/userActions'
-import {getUser} from '../actions/userActions'
-import Navbar from './reacstrap/Navbar'
+import { Button } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
+import { connect } from "react-redux";
+import { googleLogin } from "../actions/userActions";
+import { getUser } from "../actions/userActions";
+import Navbar from "./reacstrap/Navbar";
+
+import axios from 'axios';
+
+
 
 class Home extends Component {
   constructor() {
     super();
+    this.pushSample = this.pushSample.bind(this);
+    this.getToken = this.getToken.bind(this);
+    this.sendToken = this.sendToken.bind(this);
+    this.state = {
+      Conekta: null,
+      token: null
+    }
   }
 
-componentDidMount(){
-this.props.getUser();
-}
+  componentDidMount() {
+    this.props.getUser();
+  }
 
-componentDidUpdate(){
-  if(this.props.userLoading === false && this.props.user){
-  //  {this.props.history.push('/registration')}
-}
-}
+  componentDidUpdate() {
+    if (this.props.userLoading === false && this.props.user) {
+      //  {this.props.history.push('/registration')}
+    }
+  }
+
+  pushSample(event) {
+    console.log("funcion ejecutada");
+    event.preventDefault();
+    axios.post('http://10.242.0.70:1337/user', {
+      username: 'bernkaztel@hotmail.com',
+    })
+    .then(res => {
+      console.log(res);
+    });
+  }
+
+  pathSample(event){ 
+    console.log("funcion ejecutada");
+    event.preventDefault();
+    axios.patch('http://10.242.0.70:1337/user/95', {
+      "fullName": "Mariana Romo",
+    "email": "bernkaztel@hotmail.com",
+    "phone": "5584044798",
+    })
+    .then(res => {
+      console.log(res);
+    });
+  }
+
+
+
+    
+  async getToken() {
+    
+    const data = {
+      card: {
+        number: "4242424242424242",
+        name: "Javier Pedreiro",
+        exp_year: "2018",
+        exp_month: "12",
+        cvc: "123"
+      }
+    };
+
+    let stfun=this.sendToken;
+
+    
+
+    await window.Conekta.Token.create(data, 
+      function(token){
+        stfun(token);
+        
+      let token2 = token.id;
+    }, function(err){
+      console.error(err);
+    })
+
+  }
+
+
+  sendToken(token) {
+
+  let newToken = this.state.token
+  console.log(token);
+    axios.post('http://10.242.0.70:1337/payment', {
+      "token": token,
+      "amount": "20000",
+      "user": "95",
+      "service": "vip"
+    })
+    .then(res => {
+      console.log(res);
+    }).catch(err => 
+      {
+        console.log(err);
+      });
+  }
+
+
+
+  
 
   render() {
+
     return (
       <Container>
-        <Navbar/>
+        <Navbar />
         <JumbotronCard />
-        <Col md={{ size: 4, offset: 4}} className="d-block mx-auto">
-        <Button color="primary" >Inicia Sesión</Button>{' '}
-        <Button color="success" onClick={this.props.googleLogin}>Regístrate</Button>{' '}
+        <Col md={{ size: 4, offset: 4 }} className="d-block mx-auto">
+          <Button color="primary">Inicia Sesión</Button>
+          <Button color="success" onClick={this.props.googleLogin}>
+            Regístrate
+          </Button>
+          <button type="submit" onClick={this.pushSample}>
+            enviar usuario
+          </button>
+          <button type="submit" onClick={this.pathSample}>
+            llenar datos usuario
+          </button>
+          <button type="submit" onClick={this.getToken}>
+            enviar token
+          </button>
+          <button type="submit" onClick={this.sendToken}>
+            enviar orden
+          </button>
+            
         </Col>
       </Container>
+      
     );
   }
 }
 
-function mapStateToProps(state, ownProps){
+function mapStateToProps(state, ownProps) {
   return {
     user: state.user,
-    userLoading: state.loading.user,
-  }
+    userLoading: state.loading.user
+  };
 }
-export default connect(mapStateToProps,{googleLogin, getUser} )(Home);
+export default connect(mapStateToProps, { googleLogin, getUser })(Home);
--- src/components/reacstrap/Form.js
+++ src/components/reacstrap/Form.js
@@ -1,24 +1,67 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-export default class Example extends React.Component {
+export default class FormUser extends React.Component {
+  constructor(){
+    super();
+    this.onListNameChange = this.onListNameChange.bind(this);
+    this.onListEmailChange = this.onListEmailChange.bind(this);
+    this.onListPhoneChange = this.onListPhoneChange.bind(this);
+    this.pushMessage = this.pushMessage.bind(this);
+    this.state = {
+      name: "",
+      email:"",
+      phone: ""
+    }
+  }
+
+    //Cada vez que el input cambie, el valor se va a almacenar en el estado.
+    onListNameChange(event) {
+      console.log(event.target.value)
+      //Sacamos una copia del estado.
+      const nameStateCopy = event.target.value;
+      //Enviamos de vuelta el estado
+      this.setState({ name: nameStateCopy });
+    }
+
+
+        //Cada vez que el input cambie, el valor se va a almacenar en el estado.
+        onListEmailChange(event) {
+          console.log(event.target.value)
+          //Sacamos una copia del estado.
+          const emailStateCopy = event.target.value;
+          //Enviamos de vuelta el estado
+          this.setState({ email: emailStateCopy });
+        }
+
+            //Cada vez que el input cambie, el valor se va a almacenar en el estado.
+    onListPhoneChange(event) {
+      console.log(event.target.value)
+      //Sacamos una copia del estado.
+      const phoneStateCopy = event.target.value;
+      //Enviamos de vuelta el estado
+      this.setState({ phone: phoneStateCopy });
+    }
+
+   
+
   render() {
     return (
       <Form>
         <FormGroup>
-          <Label for="exampleEmail">Tu nombre</Label>
-          <Input type="email" name="email" id="exampleEmail" />
+          <Label for="exampleEmail">Nombre</Label>
+          <Input type="email" name="email" id="exampleEmail"   onChange={this.onListNameChange}/>
         </FormGroup>
         <FormGroup>
-          <Label for="examplePassword">Tu frase favorita</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+          <Label for="examplePassword">Email</Label>
+          <Input name="text" id="examplePassword" placeholder="password placeholder" onChange={this.onListEmailChange} />
         </FormGroup>
         <FormGroup>
-          <Label for="exampleFile">Sube tu foto de perfil</Label>
-          <Input type="file" name="file" id="exampleFile" />
+          <Label for="exampleFile">Teléfono</Label>
+          <Input name="text" id="examplePassword" placeholder="password placeholder" onChange={this.onListPhoneChange} />
         </FormGroup>
         <br/>
-        <Button>Submit</Button>
+        <Button onClick={this.pushMessge}>Submit</Button>
       </Form>
     );
   }
